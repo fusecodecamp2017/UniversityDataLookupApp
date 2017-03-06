9eb4dd53e606488d3c65d8f01b8abf0f3677c27b
cah-stephanhaller <stephan.haller@cardinalhealth.com>
2017-03-05 23:09:20 -0500

    Server side project created

---
+++ src/main/java/com/fusecodecamp/lookup/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.fusecodecamp.lookup.config.audit;
---
+++ src/main/java/com/fusecodecamp/lookup/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.fusecodecamp.lookup.repository;
+
+import com.fusecodecamp.lookup.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
