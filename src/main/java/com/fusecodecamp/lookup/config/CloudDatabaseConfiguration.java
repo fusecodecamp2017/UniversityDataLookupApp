1cfcc5a4cb6dad98a77e6c801526dfe274d0b9f6
cah-stephanhaller <stephan.haller@cardinalhealth.com>
2017-04-19 22:53:19 -0400

    No longer using cloud profile

--- src/main/java/com/fusecodecamp/lookup/config/CloudDatabaseConfiguration.java
+++ src/main/java/com/fusecodecamp/lookup/config/CloudDatabaseConfiguration.java
@@ -1,16 +1,16 @@
 package com.fusecodecamp.lookup.config;
 
-import io.github.jhipster.config.JHipsterConstants;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
-@Profile(JHipsterConstants.SPRING_PROFILE_CLOUD)
+@Profile(Constants.SPRING_PROFILE_NO_CLOUD)
 public class CloudDatabaseConfiguration extends AbstractCloudConfig {
 
     private final Logger log = LoggerFactory.getLogger(CloudDatabaseConfiguration.class);
--- src/main/java/com/fusecodecamp/lookup/config/Constants.java
+++ src/main/java/com/fusecodecamp/lookup/config/Constants.java
@@ -11,6 +11,8 @@ public final class Constants {
     public static final String SYSTEM_ACCOUNT = "system";
     public static final String ANONYMOUS_USER = "anonymoususer";
 
+    public static final String SPRING_PROFILE_NO_CLOUD = "nocloud";
+
     private Constants() {
     }
 }
