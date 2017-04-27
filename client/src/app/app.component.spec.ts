102a6655c7f3a7e9eda89b5db4f9e365eb708d52
cah-stephanhaller <stephan.haller@cardinalhealth.com>
2017-04-26 22:07:44 -0400

    wip

---
+++ client/src/app/app.component.spec.ts
@@ -0,0 +1,43 @@
+import { async, TestBed } from '@angular/core/testing';
+import { IonicModule } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { SearchNavController } from '../pages/search-nav-controller/search-nav-controller';
+
+import { MyApp } from './app.component';
+
+describe('MyApp Component', () => {
+  let fixture;
+  let component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [MyApp, SearchNavController],
+      imports: [
+        IonicModule.forRoot(MyApp)
+      ],
+      providers: [
+        StatusBar,
+        SplashScreen
+      ]
+    })
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyApp);
+    component = fixture.componentInstance;
+  });
+
+  it ('should be created', () => {
+    expect(component instanceof MyApp).toBe(true);
+  });
+
+  it ('should have two pages', () => {
+    expect(component.pages.length).toBe(2);
+  });
+
+  it ('page one should title should be Search Wizard', () => {
+    expect(component.pages[0].title).toEqual('Search Wizard');
+  });
+
+});
